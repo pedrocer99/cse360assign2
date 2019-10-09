562a2d064ddd77a783f88fc2d3cd5942af9e61ea
Peter <Peter@DESKTOP-UDAEHJB>
2019-10-08 17:49:54 -0700

    Added main and filled some of the methods in AddingMachine

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,28 +3,31 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String op = "0 ";
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value; //Adds the value entered to the total.
+		op = System.out.print(" + " + value);
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value; //Subtracts the value entered from the total 
+		op = System.out.print(" - " + value);
 	}
 
 	public String toString () {
-		return "";
+		return op;
 	}
 
 	public void clear() {
-	
+		total = 0; //Sets the total to zero.
 	}
 }
---
+++ cse360assign2/src/cse360assign2/Main.java
@@ -0,0 +1,11 @@
+package cse360assign2;
+
+
+public class Main {
+	
+	AddingMachine myCalculator = new AddingMachine();
+	
+	myCalculator.add(3);
+	myCalculator.add(7);
+	myCalculator.subtract(6);
+}
